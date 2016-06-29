4634af9bc780001dda5b3edac9513d5ac624ab86
Danny Piper <djpiper28@gmail.com>
2016-06-29 16:40:48 +0100

    Added files

---
+++ include/ec_version.h
@@ -0,0 +1,15 @@
+#ifndef ETTERCAP_VERS_H
+#define ETTERCAP_VERS_H
+
+#define EC_VERSION            "0.8.2"
+#define EC_VERSION_MAJOR      0
+#define EC_VERSION_MINOR      8
+#define EC_VERSION_REVISION   2
+#define EC_PROGRAM            "ettercap"
+#define EC_COPYRIGHT          "2001-2015"
+#define EC_AUTHORS            "Ettercap Development Team"
+
+#endif
+
+/* EOF */
+
